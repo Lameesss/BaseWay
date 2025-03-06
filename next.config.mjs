2d5382e15472978daa8ec45dbf19863821a615e1
Lamees <llameessss@gmail.com>
2025-03-07 05:03:11 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+     images:{
+        remotePatterns:[
+            {
+                protocol:"https",
+                hostname:"images.pexels.com",
+            },
+            {
+                protocol:"https",
+                hostname:"images.freepik.com",
+            },
+            {
+                protocol: "https",
+                hostname: "static.wixstatic.com",
+              },
+              {
+                protocol: "https",
+                hostname: "people.pic1.co",
+              },
+              {
+                protocol: "https",
+                hostname: "app-uploads-cdn.fera.ai",
+              },
+        ],
+     },
+
+   
+
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,69 @@
-const HomePage = () => {
+//"use client";
+
+import CategoriesList from "@/components/CategoriesList"
+import Categories from "@/components/CategoriesList"
+import ProductList from "@/components/ProductList"
+import Skeleton from "@/components/Skeleton"
+import Slider from "@/components/Slider"
+import { WixClientContext } from "@/context/wixContext"
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/WixClientServer"
+import { Suspense, useContext, useEffect } from "react"
+
+
+const HomePage = async () => {
+
+// const wixClient = useWixClient()
+
+
+// useEffect(() => {
+//     const getProducts = async () => {
+//       const res = await wixClient.products.queryProducts().find();
+
+//       console.log(res)
+//     };
+
+//     getProducts();
+//   }, [wixClient]);
+   
+//  const wixClient = await wixClientServer()
+//  const res = await wixClient.products.queryProducts().find();
+//console.log(res);
   return (
-    <div className=''>HomePage</div>
+    <div className=''>
+
+      <Slider/>
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+          <h1 className="text-2xl ">Featured Products</h1>
+             
+          <Suspense fallback={<Skeleton />}>
+          <ProductList
+            categoryId={process.env.FEATURED_PRODUCTS_FEATURED_CATEGORY_ID!}
+            limit={4}
+          />
+        </Suspense>
+        
+      </div>
+
+      <div className="mt-24  ">
+          <h1 className="text-2xl  px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 mb-12">Categories</h1>
+        
+        <Suspense fallback={"loading"}>
+
+        <CategoriesList/>
+        </Suspense>
+            
+      </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+          <h1 className="text-2xl ">New Products</h1>
+
+        
+             
+        
+        
+      </div>
+    </div>
   )
 }
 
